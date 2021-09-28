ea8132ef5a08d8048ea08766a62bf9ed3dc0474f
care <dev.care0717@gmail.com>
2021-09-28 21:39:55 +0900

    feat: add kernel

--- run_qemu.sh
+++ run_qemu.sh
@@ -5,12 +5,25 @@ then
     echo "Usage: $0 <.efi file> [another file]"
     exit 1
 fi
+EFI_FILE=$1
+ANOTHER_FILE=$2
+
+clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c ./kernel/main.cpp
+ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
 
 qemu-img create -f raw disk.img 200M
 mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
 mkdir -p mnt
 sudo mount -o loop disk.img mnt
 sudo mkdir -p mnt/EFI/BOOT
-sudo cp $1 mnt/EFI/BOOT/BOOTX64.EFI
+sudo cp $EFI_FILE mnt/EFI/BOOT/BOOTX64.EFI
+if [ "$ANOTHER_FILE" != "" ]
+then
+    sudo cp $ANOTHER_FILE mnt/
+fi
+sleep 0.5
 sudo umount mnt
-qemu-system-x86_64 -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd -hda disk.img
+qemu-system-x86_64 \
+    -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd \
+    -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd -hda disk.img \
+    -monitor stdio
